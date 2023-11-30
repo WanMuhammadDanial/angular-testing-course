51a02039fad3aa33861396ca447c1b4bb1d35df5
Wan Muhammad Danial Bin Seniwan <danial.seniwan@tmrnd.com.my>
2023-11-30 21:13:50 +0800

    CH 3, VID 21

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,3 +1,5 @@
+
+
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
@@ -11,62 +13,44 @@ import {setupCourses} from '../common/setup-test-data';
 
 describe('CoursesCardListComponent', () => {
 
-    let component: CoursesCardListComponent;
-    let fixture: ComponentFixture<CoursesCardListComponent>;
-    let el: DebugElement;
+  let component: CoursesCardListComponent
+  let fixture: ComponentFixture<CoursesCardListComponent>
+  let el: DebugElement;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            imports: [CoursesModule]
-        })
-        .compileComponents()
-        .then(() => {
 
-            fixture = TestBed.createComponent(CoursesCardListComponent);
-            component = fixture.componentInstance;
-            el = fixture.debugElement;
+  beforeEach(waitForAsync(()=>{
+    TestBed.configureTestingModule({
+      imports: [CoursesModule]
+    })
+    .compileComponents()
+    .then(()=>{
+      fixture = TestBed.createComponent(CoursesCardListComponent);
+      component = fixture.componentInstance;
+      el = fixture.debugElement;
+    })
+  }))
 
-        });
-    }));
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should create the component', () => {
+  it('should display the course list', () => {
 
-        expect(component).toBeTruthy();
+    component.courses = setupCourses();
 
-    });
+    const cards = el.queryAll(By.css(".course-card"))
 
-    it('should display the course list', () => {
+    expect(cards).toBeTruthy("Could not find cards")
+    expect(cards.length).toBe(12, "Unexpected number of courses")
 
-        component.courses = setupCourses();
 
-        fixture.detectChanges();
+  });
 
-        const cards = el.queryAll(By.css(".course-card"));
+  it('should display the first course', () => {
+    pending()
 
-        expect(cards).toBeTruthy("Could not find cards");
-        expect(cards.length).toBe(12, "Unexpected number of courses");
 
-    });
-
-    it('should display the first course', () => {
-
-        component.courses = setupCourses();
-
-        fixture.detectChanges();
-
-        const course = component.courses[0];
-
-        const card = el.query(By.css(".course-card:first-child")),
-                title = card.query(By.css("mat-card-title")),
-                image = card.query(By.css("img"));
-
-        expect(card).toBeTruthy("Could not find course card");
-
-        expect(title.nativeElement.textContent).toBe(course.titles.description);
-
-        expect(image.nativeElement.src).toBe(course.iconUrl);
-
-    });
+  });
 
 });
 
