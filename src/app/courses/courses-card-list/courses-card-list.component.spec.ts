4fc6da1bf084e45d3ad76aa03074b6d1cff823d9
Wan Muhammad Danial <wmdanial12@gmail.com>
2023-12-01 08:00:53 +0800

    s3v23

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,57 +1,63 @@
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { CoursesCardListComponent } from "./courses-card-list.component";
+import { CoursesModule } from "../courses.module";
+import { COURSES } from "../../../../server/db-data";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { sortCoursesBySeqNo } from "../home/sort-course-by-seq";
+import { Course } from "../model/course";
+import { setupCourses } from "../common/setup-test-data";
 
-
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import {CoursesCardListComponent} from './courses-card-list.component';
-import {CoursesModule} from '../courses.module';
-import {COURSES} from '../../../../server/db-data';
-import {DebugElement} from '@angular/core';
-import {By} from '@angular/platform-browser';
-import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
-import {Course} from '../model/course';
-import {setupCourses} from '../common/setup-test-data';
-
-
-describe('CoursesCardListComponent', () => {
-
-  let component: CoursesCardListComponent
-  let fixture: ComponentFixture<CoursesCardListComponent>
+describe("CoursesCardListComponent", () => {
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
   let el: DebugElement;
 
-
-  beforeEach(waitForAsync(()=>{
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [CoursesModule]
+      imports: [CoursesModule],
     })
-    .compileComponents()
-    .then(()=>{
-      fixture = TestBed.createComponent(CoursesCardListComponent);
-      component = fixture.componentInstance;
-      el = fixture.debugElement;
-    })
-  }))
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+      });
+  }));
 
-  it('should create the component', () => {
+  it("should create the component", () => {
     expect(component).toBeTruthy();
   });
 
-  it('should display the course list', () => {
-
+  it("should display the course list", () => {
+    // synchronous
     component.courses = setupCourses();
 
-    const cards = el.queryAll(By.css(".course-card"))
+    fixture.detectChanges();
 
-    expect(cards).toBeTruthy("Could not find cards")
-    expect(cards.length).toBe(12, "Unexpected number of courses")
+    console.log(el.nativeElement.outerHTML);
 
+    const cards = el.queryAll(By.css(".course-card"));
 
+    expect(cards).toBeTruthy("Could not find cards");
+    expect(cards.length).toBe(12, "Unexpected number of courses");
   });
 
-  it('should display the first course', () => {
-    pending()
+  it("should display the first course", () => {
+    component.courses = setupCourses();
 
+    fixture.detectChanges();
 
+    const course = component.courses[0];
+
+    const card = el.query(By.css(".course-card:first-child")),
+      title = card.query(By.css("mat-card-title")),
+      image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy("Could not find course card");
+    expect(title.nativeElement.textContent).toBeTruthy(
+      course.titles.description
+    );
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
-
 });
-
-
